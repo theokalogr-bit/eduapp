cec8fd815f4f665100758265e7fdd5f96d9c4777
theokalogr <theokalogr@gmail.com>
2026-09-13 18:17:30 +0300

    add repository interfaces for Attachment, Capability, PersonalInfo, Region, Role, Teacher, and User entities

---
+++ src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.eduapp.repository;
+
+import gr.aueb.cf.eduapp.model.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long>,
+        JpaSpecificationExecutor<Teacher> {
+
+    Optional<Teacher> findByUuid(UUID uuid);
+    Optional<Teacher> findByUuidAndDeletedFalse(UUID uuid);
+
+    Optional<Teacher> findByVat(String vat);
+    Optional<Teacher> findByVatAndDeletedFalse(String vat);
+
+    Optional<Teacher> findByPersonalInfo_Amka(String amka);
+
+    @EntityGraph(attributePaths = {"personalInfo", "region"})
+    Page<Teacher> findAllByDeletedFalse(Pageable pageable);
+
+    boolean existsByUuidAndUser_Uuid(UUID teacherUuid, UUID userUuid);
+}
---
+++ src/main/java/gr/aueb/cf/eduapp/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.eduapp.repository;
+
+import gr.aueb.cf.eduapp.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, Long>,
+        JpaSpecificationExecutor<User> {
+
+    Optional<User> findByUuid(UUID uuid);
+    Optional<User> findByUuidAndDeletedFalse(UUID uuid);
+
+    @EntityGraph(attributePaths = {"role", "role.capabilities"})
+    Optional<User> findByUsername(String username);
+
+    @EntityGraph(attributePaths = {"role", "role.capabilities"})
+    Optional<User> findByUsernameAndDeletedFalse(String username);
+
+}
